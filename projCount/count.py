92fc0a80434fd26f394202989f7e6961c6dbbec1
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2023-07-24 17:05:22 +0200

    Tool restored All working

--- projCount/count.py
+++ projCount/count.py
@@ -2,6 +2,9 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Calculator"
+    page.theme_mode = "dark"
+    page.vertical_alignment = "top"
+    page.horizontal_alignment = "center"
 
     page.fonts = {
         "RobotoMono-Regular" : "RobotoMono-Regular.ttf",
@@ -17,59 +20,48 @@ def main(page: ft.Page):
         alignment="center"
     )   
     
-    
-    
     enter_width = ft.Text("Enter Width:", size=22)
     width = ft.TextField(value="", text_align=ft.TextAlign.RIGHT, width=100)
-    row_01 = ft.Row([enter_width, width])
     enter_height = ft.Text("Enter Height:", size=22)
     height = ft.TextField(value="", text_align=ft.TextAlign.RIGHT, width=100)
-    row_02 = ft.Row([enter_height, height])
-    enter_new_width = ft.Text("New Width", size=22)
+    enter_new_width = ft.Text("New Width:", size=22)
     resWidth = ft.TextField(value="", text_align=ft.TextAlign.RIGHT, width=100)
-    row_03 = ft.Row([enter_new_width, resWidth])
-    enter_new_height = ft.Text("New Width", size=22)
+    enter_new_height = ft.Text("New Height:", size=22)
     resHeight = ft.TextField(value="", text_align=ft.TextAlign.RIGHT, width=100)
-    row_04 = ft.Row([enter_new_height, resHeight])
+    space = ft.Text("")
     viwer = ft.Row()
-    container = ft.Column([row_01, row_02, row_03, row_04])
+    col_01 = ft.Column([enter_width,space,enter_height, space, enter_new_width, space, enter_new_height],alignment="letf")
+    col_02 = ft.Column([width, height, resWidth, resHeight], alignment="center")
+    container = ft.Row([col_01, col_02], alignment="center")
+    
     
     data_container = ft.Container(
         content= container,
         width= 750,
-        height= 500,
-        bgcolor="#4d4d4d",
+        height= 300,
         border_radius=30,
         padding=20,
     )
     
-    
-    
-    
-    
-    
-    
     def clear_text(e):
-        width.value=""
-        height.value=""
-        resHeight.value=""
-        resWidth.value=""
-        viwer.clean()
+        width.value = ""
+        height.value = ""
+        resHeight.value = ""
+        resWidth.value = ""
+        viwer.controls.clear()
         page.update()
         
-    
-    
     def calculate(e):
         if resHeight.value == "":
             result = str(int(height.value) * int(resWidth.value) / int(width.value))
-            viwer.controls.append(ft.Text(f"Result: {result}", text_align=ft.TextAlign.CENTER))
+            viwer.controls.append(ft.Text(f"Result: {result}", size=40, font_family="RobotoMono-Bold"))
             page.update()
             
         elif resWidth.value == "":
             result = str(int(width.value) * int(resHeight.value) / int(height.value))
-            viwer.controls.append(ft.Text(f"Result: {result}"))
+            viwer.controls.append(ft.Text(f"Result: {result}", size=40, font_family="RobotoMono-Bold"))
             page.update()
-            
+    
     
     page.add(
         app_title,
@@ -78,11 +70,13 @@ def main(page: ft.Page):
             [
                 ft.ElevatedButton(text="Clear", on_click= clear_text),
                 ft.ElevatedButton(text="Calculate", on_click=calculate),
-
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        viwer
+            ], alignment="center"
+        ), 
+        ft.Row(
+            [
+                viwer
+            ], alignment="center"
+        )
     )
 
 ft.app(target=main, assets_dir="assets")
